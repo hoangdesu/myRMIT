2f0b3d6532d369cdb5ae9702044a44a261a5b343
s3740819 <s3740819@rmit.edu.vn>
2021-01-21 03:39:03 +0700

    welcome

---
+++ app/src/main/java/com/example/myrmit/WelcomeActivity.java
@@ -0,0 +1,76 @@
+package com.example.myrmit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Movie;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.io.InputStream;
+
+public class WelcomeActivity extends AppCompatActivity {
+    public static Activity activity;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        activity = this;
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        }, 4000);
+    }
+
+    public static class MYGIFView extends View {
+        private Movie movie;
+        long startTime;
+        @SuppressLint("ResourceType")
+        public MYGIFView(Context context) {
+            super(context);
+            init(context);
+        }
+        public MYGIFView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            init(context);
+        }
+
+        public MYGIFView(Context context, AttributeSet attrs,
+                         int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+            init(context);
+        }
+        @SuppressLint("ResourceType")
+        private void init(Context context){
+            setFocusable(true);
+            InputStream is = context.getResources().openRawResource(R.drawable.giphy);
+            movie=Movie.decodeStream(is);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.scale(((float)super.getWidth() /(float)movie.width()) ,(float)super.getHeight() /(float)movie.height());
+            canvas.drawColor(Color.WHITE);
+            super.onDraw(canvas);
+            long now=android.os.SystemClock.uptimeMillis();
+            if (startTime == 0) { // first time
+                startTime = now;
+            }
+            int relTime = (int)((now - startTime));
+            movie.setTime(relTime);
+            movie.draw(canvas,0,0);
+            this.invalidate();
+        }
+
+    }
+}
