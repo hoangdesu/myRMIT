093ff700b4f9ceb38506fcf542e3a03b102ce508
s3740819 <s3740819@rmit.edu.vn>
2021-01-26 17:07:18 +0700

    Update Courses.java

--- app/src/main/java/com/example/myrmit/coursesActivity/Courses.java
+++ app/src/main/java/com/example/myrmit/coursesActivity/Courses.java
@@ -39,115 +39,161 @@ public class Courses extends AppCompatActivity {
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private ImageView canvas;
-    OES_Fragment oes_fragment;
-    TimetableFragment timetableFragment;
-    Display display;
-    int endX;
-    boolean isStart = false;
-    AllocationFragment allocationFragment;
+    private OES_Fragment oes_fragment;
+    private ListView listview;
+    private TimetableFragment timetableFragment;
+    private int endX;
+    private boolean isStart = false;
+    private AllocationFragment allocationFragment;
     @Override
     @SuppressLint("ClickableViewAccessibility")
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            setContentView(R.layout.activity_courses);
-            oes_fragment = new OES_Fragment();
-            allocationFragment = new AllocationFragment();
-            timetableFragment = new TimetableFragment();
-            viewPager = findViewById(R.id.pager1);
-            tabLayout = findViewById(R.id.tab_layout1);
-            canvas = findViewById(R.id.imageView2);
-            display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-            tabLayout.setupWithViewPager(viewPager);
-            ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
-            viewPagerAdapter.addFragment(oes_fragment, "");
-            viewPagerAdapter.addFragment(allocationFragment, "");
-            viewPagerAdapter.addFragment(timetableFragment, "");
-            viewPager.setAdapter(viewPagerAdapter);
-
-            Objects.requireNonNull(tabLayout.getTabAt(0)).setIcon(R.drawable.oes);
-            Objects.requireNonNull(tabLayout.getTabAt(1)).setIcon(R.drawable.allocation);
-            Objects.requireNonNull(tabLayout.getTabAt(2)).setIcon(R.drawable.timetable);
-            final int[] isMove = {0};
-            canvas.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (!isStart) {
-                        endX = (int) v.getX();
-                        isStart = true;
-                    }
-                    if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                        isMove[0]++;
-                        int x_cord = (int) event.getRawX();
-                        int y_cord = (int) event.getRawY();
-                        canvas.setX(x_cord - 75);
-                        if (y_cord - 115 > 0 && y_cord - 155 < viewPager.getHeight()) {
-                            canvas.setY(y_cord - 145);
-                        } else if (y_cord - 115 < 0) {
-                            canvas.setY(0);
-                        } else canvas.setY(viewPager.getHeight());
-                    } else {
-                        if (event.getAction() != MotionEvent.ACTION_DOWN) {
-                            if ((int) v.getX() < endX / 2) {
-                                canvas.setX(0);
-                            } else canvas.setX(endX);
-                            if (isMove[0] < 4) {
-                                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.instructure.candroid");
-                                if (launchIntent != null) {
-                                    startActivity(launchIntent);
-                                } else {
-                                    Toast.makeText(Courses.this, "There is no package available in android", Toast.LENGTH_LONG).show();
-                                    final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
-                                    try {
-                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + "com.instructure.candroid")));
-                                    } catch (android.content.ActivityNotFoundException anfe) {
-                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + "com.instructure.candroid")));
-                                    }
-                                }
-                            }
-                            isMove[0] = 0;
-                        }
-                    }
-                    return true;
-                }
-            });
+            accountView();
         }
         else {
-            FirebaseHandler firebaseHandler = new FirebaseHandler();
-            setContentView(R.layout.activity_courses_guess);
-            ListView listview = findViewById(R.id.courseView);
-            firebaseHandler.getRMITPrograms().get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    ArrayList<CourseReview> list = new ArrayList<>();
-                    for (DocumentSnapshot documentSnapshot: task.getResult()){
-                        String description = (String) documentSnapshot.get("description");
-                        String[] temp = description.split("~");
-                        description = "";
-                        for (int i = 0; i < temp.length; i++){
-                            if (i != temp.length-1){
-                                description += temp[i] + "\n";
-                            }
-                            else description += temp[i];
-                         }
-                        list.add(get(documentSnapshot.getId(), (String)documentSnapshot.get("name"),description, (ArrayList<String>) documentSnapshot.get("courses")));
-                    }
-                    if (list.size() == task.getResult().size()){
-                        ArrayAdapterCourses arrayAdapter = new ArrayAdapterCourses(Courses.this, list);
-                        listview.setAdapter(arrayAdapter);
-                    }
-                }
-            });
+            guessView();
         }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private void accountView(){
+        setContentView(R.layout.activity_courses);
+        initialSetting();
+
+        // Setup view adapter
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), 0);
+        viewPagerAdapter.addFragment(oes_fragment, "");
+        viewPagerAdapter.addFragment(allocationFragment, "");
+        viewPagerAdapter.addFragment(timetableFragment, "");
+        viewPager.setAdapter(viewPagerAdapter);
+
+        // Set icon for all fragments
+        Objects.requireNonNull(tabLayout.getTabAt(0)).setIcon(R.drawable.oes);
+        Objects.requireNonNull(tabLayout.getTabAt(1)).setIcon(R.drawable.allocation);
+        Objects.requireNonNull(tabLayout.getTabAt(2)).setIcon(R.drawable.timetable);
+
+        // Set canvas access
+        setCanvasAccess();
+    }
+
+    /**
+     * Initial setting for RMIT account
+     */
+    private void initialSetting(){
+        oes_fragment = new OES_Fragment();
+        allocationFragment = new AllocationFragment();
+        timetableFragment = new TimetableFragment();
+        viewPager = findViewById(R.id.pager1);
+        tabLayout = findViewById(R.id.tab_layout1);
+        canvas = findViewById(R.id.imageView2);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    /**
+     * Set onclick for the canvas icon to move to the app canvas
+     */
+    @SuppressLint("ClickableViewAccessibility")
+    private void setCanvasAccess(){
+        final int[] isMove = {0};
+        canvas.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (!isStart) {
+                    endX = (int) v.getX();
+                    isStart = true;
+                }
+                if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                    isMove[0]++;
+                    int x_cord = (int) event.getRawX();
+                    int y_cord = (int) event.getRawY();
+                    canvas.setX(x_cord - 75);
+                    if (y_cord - 115 > 0 && y_cord - 155 < viewPager.getHeight()) {
+                        canvas.setY(y_cord - 145);
+                    } else if (y_cord - 115 < 0) {
+                        canvas.setY(0);
+                    } else canvas.setY(viewPager.getHeight());
+                } else {
+                    if (event.getAction() != MotionEvent.ACTION_DOWN) {
+                        if ((int) v.getX() < endX / 2) {
+                            canvas.setX(0);
+                        } else canvas.setX(endX);
+                        if (isMove[0] < 4) {
+                            Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.instructure.candroid");
+                            if (launchIntent != null) {
+                                startActivity(launchIntent);
+                            } else {
+                                Toast.makeText(Courses.this, "There is no package available in android", Toast.LENGTH_LONG).show();
+                                final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                                try {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + "com.instructure.candroid")));
+                                } catch (android.content.ActivityNotFoundException anfe) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + "com.instructure.candroid")));
+                                }
+                            }
+                        }
+                        isMove[0] = 0;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Setup for guess account to view available programs
+     */
+    private void guessView(){
+        // Initial setup
+        FirebaseHandler firebaseHandler = new FirebaseHandler();
+        setContentView(R.layout.activity_courses_guess);
+        listview = findViewById(R.id.courseView);
+
+        // get all the undergraduate programs
+        firebaseHandler.getRMITPrograms().get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {         // Set list view
+                ArrayList<CourseReview> list = new ArrayList<>();
+                for (DocumentSnapshot documentSnapshot: task.getResult()){      // Set all detail with each program
+                    list.add(get(documentSnapshot.getId(), (String)documentSnapshot.get("name"),getDescription(documentSnapshot), (ArrayList<String>) documentSnapshot.get("courses")));
+                }
+                if (list.size() == task.getResult().size()){                    // finalize the list
+                    ArrayAdapterCourses arrayAdapter = new ArrayAdapterCourses(Courses.this, list);
+                    listview.setAdapter(arrayAdapter);
+                }
+            }
+        });
+    }
+
+    /**
+     * Get description from firebase
+     * @param documentSnapshot DocumentSnapshot
+     * @return String
+     */
+    private String getDescription(DocumentSnapshot documentSnapshot){
+        String description = (String) documentSnapshot.get("description");
+        String[] temp = description.split("~");
+        description = "";
+        for (int i = 0; i < temp.length; i++){
+            if (i != temp.length-1){
+                description += temp[i] + "\n";
+            }
+            else description += temp[i];
+        }
+        return description;
+    }
+
+    /**
+     * Get CourseView for guess account
+     * @param code String
+     * @param name String
+     * @param description String
+     * @param courses ArrayList<String>
+     * @return CourseReview
+     */
     private CourseReview get(String code, String name, String description, ArrayList<String> courses){
         return new CourseReview(name, description, courses, code);
     }
-
-    public void back(View view){
-        finish();
-    }
-
 
 }
